dcb5062c760689c74baba79a8695de9dc9290b38
Daniel Lemky <daniel.lemky@moxiworks.com>
2025-02-09 10:58:50 -0600

    autodark mode

--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -5,7 +5,6 @@ return {
   priority = 1000, -- make sure to load this before all the other start plugins
   -- Optional; default configuration will be used if setup isn't called.
   config = function()
-    vim.cmd.colorscheme "catppuccin-mocha"
     require("everforest").setup({
     transparent_background_level = 2
     })
