f2f7967d54088534fe1e07251a7f95be43f94e02
Daniel Lemky <daniel.lemky@moxiworks.com>
2025-04-08 15:21:57 -0500

    augment code keymaps

--- lua/plugins/augment-code.lua
+++ lua/plugins/augment-code.lua
@@ -1,6 +1,8 @@
 return {
   'augmentcode/augment.vim',
   config = function()
-    vim.keymap.set("n", "<leader>a", "<cmd>Augment chat<CR>")
+    vim.keymap.set("n", "<leader>am", "<cmd>Augment chat<CR>", { desc = "Send message to Augment" })
+    vim.keymap.set("n", "<leader>an", "<cmd>Augment chat-new<CR>", { desc = "Create new Augment chat" })
+    vim.keymap.set("n", "<leader>at", "<cmd>Augment chat-toggle<CR>", { desc = "Toggle Augment chat window" })
   end
 }
